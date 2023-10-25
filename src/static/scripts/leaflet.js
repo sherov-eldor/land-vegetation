3dd859416f8926f881932be6810783a3d9f4b27a
sherov-eldor <eldor.sherov.97@inbox.ru>
2023-10-25 17:58:13 +0300

    second commit

--- src/static/scripts/leaflet.js
+++ src/static/scripts/leaflet.js
@@ -3,7 +3,7 @@ var map = L.map("map", {
 }).setView([41.311081, 69.240562], 13);
 
 L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-    maxZoom: 20,
+    maxZoom: 18,
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
 
